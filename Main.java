6ffb7a1ea69d96c94260bd0c216c5250512e883c
Светка <sveta.chakrygina@mail.ru>
2022-11-03 21:21:37 +0300

    Домашнее задание №1

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import Transport.Car;
+import Transport.bus;
 import Transport.train;
 
 public class  Main {
@@ -17,6 +18,8 @@ public class  Main {
         kiaSportage4.printCar();
         hyundaiAvante.printCar();
 
+        System.out.println(" ");
+
 
         train lastochka = new train("Ласточка", "В-901", 2011, "России", null, 301, 3500, 6.55, "Белорусский вокзал", "Минск - Пассажирский", 11);
         train leningrad = new train("Ленинград", "D-125", 2019, "России", null, 270, 1700, 8.49, "Ленинградский вокзал","Ленинград - Пассажирский",8);
@@ -24,6 +27,16 @@ public class  Main {
         lastochka.printTrain();
         leningrad.printTrain();
 
+        System.out.println(" ");
+
+
+        bus gaz = new bus("Газель NEXT", "A65R25-60", 2021,"России", "черный", 120, 30, "Академика Сахарова", "Московский вокзал", 1.35);
+        bus liaz = new bus("Лиаз-", "5292 рестайлинг", 2020, "России", "голубой", 90, 28, "Кузнечиха-2", "Площадь Горького", 1.5);
+        bus man = new bus("MAN", "49", 2018, "России", "белый", 80, 2000, "Чебоксары", "Казань", 3);
+
+        gaz.printBus();
+        liaz.printBus();
+        man.printBus();
 
     }
 }
---
+++ Transport/bus.java
@@ -0,0 +1,51 @@
+package Transport;
+
+ public class bus extends transport {
+     public int ticketPrice;
+     private String busStation;
+     private String busFinalStation;
+     public double busTravelTime;
+
+
+     public bus(String brand, String model, int productionYear, String productionCountry, String color,
+                int speed, int ticketPrice, String busStation, String busFinalStation, double busTravelTime) {
+         super(brand, model, productionYear, productionCountry, color, speed);
+         this.ticketPrice = ticketPrice;
+         this.busStation = busStation;
+         this.busFinalStation = busFinalStation;
+         this.busTravelTime = busTravelTime;
+     }
+
+
+         public String getBusStation() {
+             return busStation;
+         }
+
+         public String getBusFinalStation() {
+             return busFinalStation;
+         }
+
+     public int getTicketPrice() {
+         return ticketPrice;
+     }
+
+     public void setTicketPrice(int ticketPrice) {
+         this.ticketPrice = ticketPrice;
+     }
+
+     public double getBusTravelTime() {
+         return busTravelTime;
+     }
+
+     public void setBusTravelTime(double busTravelTime) {
+         this.busTravelTime = busTravelTime;
+     }
+
+     public void printBus() {
+         System.out.println("Автобус " + getBrand() + " " + "модель " + getModel() + ", " + getProductionYear() +
+                 " год выпуска в " + getProductionCountry() + ", цвет : " + getColor() + ", скорость: " + getSpeed() +
+                 " , начальная остановка: " + getBusStation() + " и следует до остановки: " + getBusFinalStation() + ". Цена поездки: " + getTicketPrice());
+
+     }
+ }
+
